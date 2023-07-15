b359aaf884ca4421147f772c045ec638330de6cf
gaju-01 <gajananchegde01@gmail.com>
2023-07-15 22:52:34 +0530

    [Localhost Refused To Connect Exception Resolved]

--- src/main/java/RESTAPI/SocialMediaRestAPI/Security/SpringSecurityConfiguration.java
+++ src/main/java/RESTAPI/SocialMediaRestAPI/Security/SpringSecurityConfiguration.java
@@ -1,19 +1,21 @@
 package RESTAPI.SocialMediaRestAPI.Security;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.web.SecurityFilterChain;
-import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfiguration {
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(
-                auth -> auth.anyRequest().authenticated());
-        http.httpBasic(withDefaults());
-        http.csrf().disable();
-        return http.build();
-    }
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+    http.httpBasic(withDefaults());
+    http.csrf().disable();
+    http.headers().frameOptions().disable();
+    return http.build();
+  }
 }
